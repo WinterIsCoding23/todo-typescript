73a602ced259cda1affe57f4cb0f8ce26e8675ef
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-31 18:00:24 +0200

    create the 2 main-divs header & todoList

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import React, { FC } from "react";
 import "./App.css";
 
 const App: FC = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="header"></div>
+      <div className="todoList"></div>
+    </div>
+  );
 };
 
 export default App;
