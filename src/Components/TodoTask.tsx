91be474361db8c4a11de9d9630a7ab8200bd825a
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-31 21:28:31 +0200

    define completeTask-function & pass it as props to TodoTask

--- src/Components/TodoTask.tsx
+++ src/Components/TodoTask.tsx
@@ -5,16 +5,23 @@ interface Props {
   // '?' marks an optional prop
   // task?: ITask;
   task: ITask;
+  completeTask(taskNameToDelete: string): void;
 }
 
-const TodoTask = ({ task }: Props) => {
+const TodoTask = ({ task, completeTask }: Props) => {
   return (
     <div className="task">
       <div className="content">
         <span>{task.taskName}</span>
         <span>{task.deadline}</span>
       </div>
-      <button>X</button>
+      <button
+        onClick={() => {
+          completeTask(task.taskName);
+        }}
+      >
+        X
+      </button>
     </div>
   );
 };
