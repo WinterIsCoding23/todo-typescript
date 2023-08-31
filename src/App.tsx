aec6f09447a2e998d47d090ecad826ab576bd2c6
Markus Winter <markusdominicwinter@hotmail.com>
2023-08-31 18:59:43 +0200

    import useState & define task, deadline and todo-States

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import "./App.css";
 
 const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todo, setTodoList] = useState([]);
+
   return (
     <div className="App">
       <div className="header">
